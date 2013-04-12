730d90c24a627c92e66056bb58ca1fd5f959e549
gprokask <gprokask@gmail.com>
2013-04-12 15:10:31 +0200

    added Authentication

---
+++ src/main/java/de/gp/pms/shared/auth/AuthenticationFailedException.java
@@ -0,0 +1,9 @@
+package de.gp.pms.shared.auth;
+
+import java.io.Serializable;
+
+public class AuthenticationFailedException extends Exception implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
